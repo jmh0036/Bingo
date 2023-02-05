189b3494547638f8f9a7dfed47c2cb717e856265
James M. Hammer, III <james.hammer3@gmail.com>
2023-02-05 17:10:14 -0500

    Add Base Bingo Files

---
+++ Bingo.py
@@ -0,0 +1,66 @@
+import random
+
+# # Traditional Bingo: balls are numbered from 1 to 75 and the board is labeled BINGO
+balls = range(1,76)
+bingo_letters = ['B', 'I', 'N', 'G', 'O']
+
+# # Music Trivia Bingo. The length of the balls should be divisible by the length of the bingo letters.
+# balls = ['Foo Fighters', 'Guns N Roses', 'Rock3', 'Rock4', 'Rock5',
+#          'Reba', 'Dolly Parton', 'Country3', 'Country4', 'Country5',
+#          'Nirvana', 'Pearl Jam', 'Grunge3', 'Grunge4', 'Grunge5',
+#          'Michael Jackson', 'Madonna', 'Pop3', 'Pop4', 'Pop5',
+#          'Tracy Chapman', 'Ke$ha', 'PowerWomen3', 'PowerWomen4', 'PowerWomen5',]
+# bingo_letters = ['Rock', 'Country', 'Grunge', 'Pop', 'PowerWomen']
+
+if len(balls)%len(bingo_letters) != 0:
+    print('The number number of classes needs to divide the number of balls.')
+    quit()
+
+# Create an even dispersion of the balls into the letters
+number_association = { }
+board_length = len(bingo_letters)
+number_of_balls = len(balls)
+for i in range(len(bingo_letters)):
+    for j in range( i*(number_of_balls//board_length)+1,  (i+1)*(number_of_balls//board_length)+1 ):
+        number_association[balls[j-1]] = bingo_letters[i]
+# print(number_association)
+
+# Make a random calling 
+call_order = random.sample(balls, k=len(balls))
+
+# # If you want to see the whole order, you can comment out one of these.
+# print(call_order)
+# for i in call_order:
+#     print(number_association[i], i)
+
+print('Press any key for the next number or b when someone has bingo')
+next_step = input()
+numbers_called = []
+while (len(call_order) > 0 and next_step != 'b' and next_step != 'B'):
+    number = call_order.pop(0)
+    column = number_association[number]
+    print('Call: ', column, ' - ', number, '\n')
+    numbers_called.append(number)
+    print('Press any key for the next number or b when someone has bingo')
+    next_step = input()
+    print(' ')
+    if (next_step == 'b' or next_step == 'B'):
+        ordered_list = [number_association[i] + ' - ' + str(i) for i in sorted(numbers_called)]
+        print(sorted(ordered_list))
+        print('Was there indeed a bingo? Y or N.')
+        bingo_check = input()
+        print('')
+        while bingo_check not in ['Y', 'y', 'N', 'n']:
+            print('Unknown entry. Does somebody Have Bingo? Please enter Y or N.')
+            print(ordered_list)
+            bingo_check = input()
+        if (bingo_check == 'Y' or bingo_check == 'y'):
+            next_step = 'b'
+        else:
+            next_step = 'n'
+
+if len(call_order) > 0:
+    print(ordered_list)
+    print('Congratulations!')
+else:
+    print('All of the numbers have been called. Something must have went awry.')
---
+++ BingoBoard.py
@@ -0,0 +1,64 @@
+import random
+import json
+
+# # Traditional Bingo: balls are numbered from 1 to 75 and the board is labeled BINGO
+bingo_letters = ['B', 'I', 'N', 'G', 'O']
+balls = range(1,76)
+
+# # Music Trivia Bingo. The length of the balls should be divisible by the length of the bingo letters.
+# balls = ['Foo Fighters', 'Guns N Roses', 'Rock3', 'Rock4', 'Rock5',
+#          'Reba', 'Dolly Parton', 'Country3', 'Country4', 'Country5',
+#          'Nirvana', 'Pearl Jam', 'Grunge3', 'Grunge4', 'Grunge5',
+#          'Michael Jackson', 'Madonna', 'Pop3', 'Pop4', 'Pop5',
+#          'Tracy Chapman', 'Ke$ha', 'PowerWomen3', 'PowerWomen4', 'PowerWomen5',]
+# bingo_letters = ['Rock', 'Country', 'Grunge', 'Pop', 'PowerWomen']
+
+if len(balls)%len(bingo_letters) != 0:
+    print('The number number of classes needs to divide the number of balls.')
+    quit()
+
+# Create an even dispersion of the balls into the letters
+number_association = { }
+board_length = len(bingo_letters)
+number_of_balls = len(balls)
+for i in range(board_length):
+    letter_range = []
+    for j in range( i*(number_of_balls//board_length),  (i+1)*(number_of_balls//board_length) ):
+        letter_range.append(balls[j])
+    number_association[bingo_letters[i]] = letter_range
+# print(number_association)
+
+board = []
+if board_length%2 != 0:
+    for i in range(board_length):
+        if i == board_length//2:
+            board.append(random.sample(number_association[bingo_letters[i]], k=board_length//2)+['Free Space']+random.sample(number_association[bingo_letters[i]], k=board_length//2))
+        else:
+            board.append(random.sample(number_association[bingo_letters[i]], k=board_length))
+else:
+    for i in range(len(bingo_letters)):
+        board.append(random.sample(number_association[bingo_letters[i]], k=len(bingo_letters)))
+    
+for i in range(len(board)):
+    print([board[j][i] for j in range(len(board))])
+    
+class Cell:
+    def __init__(self, value: str, id: int, row: int, col: int) -> None:
+        self.value = value
+        self.id = id
+        self.row = row
+        self.col = col
+        
+# Create JSON file for the board
+JSONboard = []
+idNo = 0
+for i in range(len(board)):
+    for j in range(len(board)):
+        JSONboard.append(Cell(board[j][i],idNo,i,j,))
+        idNo += 1
+        
+jsonStr = json.dumps([ob.__dict__ for ob in JSONboard], indent=2)
+
+jsonFile = open("test_bingo_board.json", "w")
+jsonFile.write(jsonStr)
+jsonFile.close()
